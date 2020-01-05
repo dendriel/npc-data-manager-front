618cc6ddd0c5ce3e429a263c48cb268f75e16b2d
vitor.rozsa <vitor.rozsa@hotmail.com>
2020-01-05 16:29:32 -0300

    Update npc edit component

--- app/modules/npc-handler/edit/npchandler-edit.component.js
+++ app/modules/npc-handler/edit/npchandler-edit.component.js
@@ -1,18 +1,45 @@
 'use strict';
 
-function NpcHandlerEditController($routeParams, NpcHandlerService, sharedData) {
+function NpcHandlerEditController($routeParams, NpcHandlerService, sharedData, FeedbackBarService) {
     let self = this;
     self.npcData = {};
 
     console.log("Edit npc: " + sharedData.getParam("npc"));
     self.npcData = sharedData.getParam("npc");
 
+    self.addEventState = (eventsState) => {
+        eventsState.push({
+            type: "SWITCH"
+        })
+    };
+
+    self.addElementToArray = (arr, elem) => {
+        arr.push(elem);
+    };
+
+    self.removeElementFromArray = (arr, elem) => {
+        const index = arr.indexOf(elem);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+        else {
+            FeedbackBarService.error("Couldn't remove element from array.")
+        }
+    };
+
     self.save = () => {
         NpcHandlerService
             .save(self.npcData)
             .then((res) => {
-                console.log("Save response: " + res);
-            });
+                if (res.status === 200) {
+                    FeedbackBarService.info("NPC saved successfully!");
+                } else {
+                    FeedbackBarService.error("Failed to save NPC! Status: " + res.status);
+                }
+            })
+            .catch(reason =>
+                FeedbackBarService.error("Failed to save NPC! Status: " + reason.status + ". Error: " + reason.data.error)
+            );
     };
 }
 
@@ -20,5 +47,5 @@ angular
     .module('npchandler')
     .component('npcEdit', {
         templateUrl: 'modules/npc-handler/edit/npchandler-edit.template.html',
-        controller: ['$routeParams', 'NpcHandlerService', 'NpcHandlerSharedDataService', NpcHandlerEditController]
+        controller: ['$routeParams', 'NpcHandlerService', 'NpcHandlerSharedDataService', "FeedbackBarService", NpcHandlerEditController]
     });
