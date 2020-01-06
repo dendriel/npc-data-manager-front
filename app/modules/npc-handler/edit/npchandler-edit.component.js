2488febe820b12d611d8a1e8d2da90f9198d192a
vitor.rozsa <vitor.rozsa@hotmail.com>
2020-01-06 07:31:47 -0300

    Make NPC edit view fully functional

--- app/modules/npc-handler/edit/npchandler-edit.component.js
+++ app/modules/npc-handler/edit/npchandler-edit.component.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function NpcHandlerEditController($routeParams, NpcHandlerService, sharedData, FeedbackBarService) {
+function NpcHandlerEditController($scope, $routeParams, NpcHandlerService, sharedData, FeedbackBarService) {
     let self = this;
     self.npcData = {};
 
@@ -27,6 +27,68 @@ function NpcHandlerEditController($routeParams, NpcHandlerService, sharedData, F
         }
     };
 
+    self.addMessage = (arr) => {
+        let msg = {
+            title: {
+                text: "",
+                font: "Serif",
+                style: 0,
+                size: 22,
+                color: {r:255, g:255, b:255},
+                rect: {x:12, y:4, width:920, height:32},
+                verticalAlignment: 0,
+                horizontalAlignment: 2
+            },
+            text: {
+                text: "",
+                font: "Serif",
+                style: 0,
+                size: 20,
+                color: {r:255, g:255, b:255},
+                rect: {x:16, y:32, width:920, height:64},
+                verticalAlignment: 1,
+                horizontalAlignment: 2
+            }
+        };
+
+        self.addElementToArray(arr, msg);
+    };
+
+    self.addInteraction = () => {
+        let interId = self.findNextInteractionId();
+        let inter = {
+            id: interId,
+            type: "DIALOG",
+            messages: [],
+            priceMultiplier: 0,
+            items: [],
+            requireEventsState: [],
+            updateEventsState: [],
+            targetId: 0,
+            toPos: {x:0, y:0},
+            targetData: ""
+        };
+
+        self.addElementToArray(self.npcData.interactionData, inter);
+        self.addElementToArray(self.npcData.interactionOrder, interId);
+    };
+
+    self.removeInteraction = (inter) => {
+        self.removeElementFromArray(self.npcData.interactionOrder, inter.id);
+        self.removeElementFromArray(self.npcData.interactionData, inter);
+    };
+
+    self.findNextInteractionId = () => {
+        let lastId = 0;
+        self.npcData.interactionData.map(function(obj){
+            if (obj.id > lastId){
+                lastId = obj.id;
+            }
+        });
+        let nextId = lastId+1;
+        return nextId;
+    };
+
     self.save = () => {
         NpcHandlerService
             .save(self.npcData)
@@ -41,11 +103,38 @@ function NpcHandlerEditController($routeParams, NpcHandlerService, sharedData, F
                 FeedbackBarService.error("Failed to save NPC! Status: " + reason.status + ". Error: " + reason.data.error)
             );
     };
+
+    self.getSortedInteractionData = () => {
+        let sortedInteractionData = [];
+
+        let i = 0;
+        for (; i < self.npcData.interactionOrder.length; i++) {
+            let targetId = self.npcData.interactionOrder[i];
+            let currInter = self.npcData.interactionData.find(elem => elem.id === targetId);
+            sortedInteractionData.push(currInter);
+        }
+
+        return sortedInteractionData;
+    }
+
+    // $scope.$watchCollection('$ctrl.npcData.interactionOrder', function (newOrder, oldValue) {
+    //     let sortedInteractionData = [];
+    //
+    //     let i = 0;
+    //     for (; i < newOrder.length; i++) {
+    //         let targetId = newOrder[i];
+    //         let currInter = self.npcData.interactionData.find(elem => elem.id === targetId);
+    //         sortedInteractionData.push(currInter);
+    //     }
+    //
+    //     self.npcData.interactionData = sortedInteractionData;
+    //     console.log("Scope changed! " + newOrder + " - " + oldValue);
+    // });
 }
 
 angular
     .module('npchandler')
     .component('npcEdit', {
         templateUrl: 'modules/npc-handler/edit/npchandler-edit.template.html',
-        controller: ['$routeParams', 'NpcHandlerService', 'NpcHandlerSharedDataService', "FeedbackBarService", NpcHandlerEditController]
+        controller: ['$scope', '$routeParams', 'NpcHandlerService', 'NpcHandlerSharedDataService', "FeedbackBarService", NpcHandlerEditController]
     });
