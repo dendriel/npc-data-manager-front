7ef1cec2f4171591a0b13caf92f7afc4d7ca50cc
vitor.rozsa <vitor.rozsa@hotmail.com>
2020-01-26 08:31:31 -0300

    Use window localStorage to persist data between refreshes

--- app/modules/core/core-shareddata.service.js
+++ app/modules/core/core-shareddata.service.js
@@ -1,22 +1,25 @@
 'use strict';
 
-function CoreSharedDataService() {
+function CoreSharedDataService($window) {
     let self = this;
     self.data = [];
 
     self.setParam = (key, value) => {
-        self.data[key] = value;
+        // self.data[key] = value;
+        $window.localStorage.setItem(key,  angular.toJson(value));
     };
 
     self.getParam = (key) => {
-        return self.data[key];
+        //return self.data[key];
+        return  angular.fromJson($window.localStorage.getItem(key));
     };
 
     self.clear = () => {
-        self.data = [];
+        // self.data = [];
+        $window.localStorage.clear();
     }
 }
 
 angular
     .module('core')
-    .service('CoreSharedDataService', [CoreSharedDataService]);
+    .service('CoreSharedDataService', ['$window', CoreSharedDataService]);
